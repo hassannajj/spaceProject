fa67b933fa2e51b40bf437982cf631e6496b66ea
Hassan <hhalnajj@uci.edu>
2023-02-27 12:46:06 -0800

    start

---
+++ script.js
@@ -0,0 +1,45 @@
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+const img4 = document.getElementById('img4');
+const img5 = document.getElementById('img5');
+const img6 = document.getElementById('img6');
+const img7 = document.getElementById('img7');
+const img8 = document.getElementById('img8');
+
+
+
+function gammaRay() {
+    img1.toggleClass('show');
+}
+
+function xRay() {
+    console.log("xray")
+
+}
+
+function visibleLight() {
+    console.log("v light")
+
+}
+
+function infrared() {
+    console.log("infrared")
+
+}
+
+function microwave() {
+    console.log("microwave")
+
+}
+
+function radioWave() {
+    console.log("radio wave")
+
+
+}
+
+function combined() {
+    console.log("combined")
+
+}
